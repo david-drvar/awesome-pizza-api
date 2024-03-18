55732dcb0cc55d3fadc54a462adc6198b9c66f3a
david-drvar <david.drvar.bogdanovic@gmail.com>
2024-03-18 13:38:05 +0100

    project initialization

---
+++ src/main/java/com/awesomepizza/awesomepizzaapi/AwesomePizzaApiApplication.java
@@ -0,0 +1,13 @@
+package com.awesomepizza.awesomepizzaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwesomePizzaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwesomePizzaApiApplication.class, args);
+	}
+
+}
