94937d8365cd1498487b0247a4264532a14b1139
david-drvar <david.drvar.bogdanovic@gmail.com>
2024-03-19 23:17:34 +0100

    .env file configuration for storing sensitive db info

--- src/main/java/com/awesomepizza/awesomepizzaapi/AppConfig.java
+++ src/main/java/com/awesomepizza/awesomepizzaapi/AppConfig.java
@@ -3,9 +3,11 @@ package com.awesomepizza.awesomepizzaapi;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-
+@PropertySource("classpath:application.properties")
+@PropertySource("classpath:.env")
 public class AppConfig {
     @Bean
     public ModelMapper modelMapper() {
